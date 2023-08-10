7c8001efe2b979d668f4097f66514c1cb9400575
Vasiliy <wudgexyz@gmail.com>
2023-08-10 15:19:11 +0300

    Order model

---
+++ src/Services/GetMeThere.API/Models/TaxiOrder.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GetMeThere.API.Models
+{
+    public class TaxiOrder
+    {
+        [Key]
+        public Guid OrderId { get; set; }
+        public Guid ClientId { get; set; }
+        public string? ClientName { get; set; }
+        public string? PickupAddress { get; set; }
+        //address coords
+        public float PickupLatitude { get; set; }
+        public float PickupLongitude { get; set; }
+
+        public string? DestinationAddress { get; set; } // string + coords
+        public float DestinationLatitude { get; set; }
+        public float DestinationLongitude { get; set; }
+
+        public DateTime PickupTime { get; set; }
+        public float OrderPrice { get; set; } // km * defaultTaxPerKm = 7 * 25 = 170
+        //public string? ClientPhoneNumber { get; set; } // yes or not
+        public bool IsCompleted { get; set; }
+        public bool IsConfirmed { get; set; }
+
+    }
+}
--- src/Services/GetMeThere.API/Services/UserService.cs
+++ src/Services/GetMeThere.API/Services/UserService.cs
@@ -1,13 +1,15 @@
 ﻿using GetMeThere.API.Models;
 using GetMeThere.API.Models.DTO;
+using GetMeThere.API.Repositories;
 
 namespace GetMeThere.API.Services
 {
     public class UserService : IUserService
     {
-        public UserService()
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
         {
-            
+            _userRepository = userRepository;
         }
 
         public UserDto GetUserInfo(string username)
