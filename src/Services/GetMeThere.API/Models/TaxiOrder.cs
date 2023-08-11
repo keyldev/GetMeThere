2b8846d037c34b43fa819ba52b644d2e69646d39
Vasiliy <wudgexyz@gmail.com>
2023-08-11 18:04:17 +0300

    TaxiOrder.cs: Boolean values changed to false by default.

--- src/Services/GetMeThere.API/Models/TaxiOrder.cs
+++ src/Services/GetMeThere.API/Models/TaxiOrder.cs
@@ -13,19 +13,19 @@ namespace GetMeThere.API.Models
         public double PickupLatitude { get; set; }
         public double PickupLongitude { get; set; }
 
-        public string? DestinationAddress { get; set; } // string + coords
-        public double DestinationLatitude { get; set; }
-        public double DestinationLongitude { get; set; }
+        public string? DestinationAddress { get; set; }// string + coords
+        public double DestinationLatitude { get; set; } = 0.0;
+        public double DestinationLongitude { get; set; } = 0.0;
 
-        public VehicleType VehicleType { get; set; } // 0 standart, 1 prem, 2 lux
+        public VehicleType VehicleType { get; set; } = VehicleType.Standart; // 0 standart, 1 prem, 2 lux
 
-        public DateTime PickupTime { get; set; }
+        public DateTime PickupTime { get; set; } = DateTime.Now;
         public float OrderPrice { get; set; } // km * defaultTaxPerKm + priceByCarType = 7 * 25 + 100 = 270
         //public string? ClientPhoneNumber { get; set; } // yes or not
-        public bool IsCompleted { get; set; }
-        public bool IsConfirmed { get; set; }
-        public bool NeedChildSeat { get; set; } // 
-        public int SeatsCount { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public bool IsConfirmed { get; set; } = false;
+        public bool NeedChildSeat { get; set; } = false; //
+        public int SeatsCount { get; set; } = 2;
 
     }
 }
