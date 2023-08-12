86b575afd682c142d0c2e753ec621c05fb5cdd68
Vasiliy <wudgexyz@gmail.com>
2023-08-13 01:18:49 +0300

    Vehicle controller impl

---
+++ src/Services/GetMeThere.API/Controllers/VehicleController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetMeThere.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+    }
+}
