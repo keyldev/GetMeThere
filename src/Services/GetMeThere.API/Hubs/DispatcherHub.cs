d9e051833548c0fbb914b81ba73a6e9ac1150aa2
Vasiliy <wudgexyz@gmail.com>
2023-08-20 20:10:22 +0300

    DispatcherHub updates;

--- src/Services/GetMeThere.API/Hubs/DispatcherHub.cs
+++ src/Services/GetMeThere.API/Hubs/DispatcherHub.cs
@@ -4,5 +4,15 @@ namespace GetMeThere.API.Hubs
 {
     public class DispatcherHub : Hub
     {
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
     }
 }
